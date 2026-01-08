78f77fd5563f9bd3777c6ef6ee4a67b3667c9a79
Kubsv47 <134735570+Kubsv47@users.noreply.github.com>
2026-01-08 11:17:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const repoName = 'GrassBar-Site'
+
 export default defineConfig({
+  base: `/${repoName}/`,      
   plugins: [react()],
-  server: {
-    proxy: {
-      // Każde zapytanie zaczynające się od /api zostanie przekierowane
-      '/api': {
-        target: 'https://v3.football.api-sports.io', // Docelowy serwer API
-        changeOrigin: true, // Niezbędne do poprawnego działania proxy
-        rewrite: (path) => path.replace(/^\/api/, ''), // Usuwa '/api' z początku ścieżki
-      },
-    },
-  },
 })
